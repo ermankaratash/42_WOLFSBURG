68e6aeb59bffa59bcdcd6b4b594761faf13bfd37
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2022-02-10 21:57:39 +0100

    first commit

---
+++ C/00/ex05/ft_print_comb.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_commas(void)
+{
+	ft_putchar(',');
+	ft_putchar(' ');
+}
+
+void	ft_print_comb(void)
+{
+	int	num[3];
+    /* defined and array with 3 digit=[,,]*/
+
+	num[0] = '0';
+    /*[0,,]*/
+	while (num[0] <= '9')
+	{
+		num[1] = '0';
+        /*[0,0,]*/
+		while (num[1] <= '9')
+		{
+			num[2] = '0';
+            /*[0,0,0]*/
+			while (num[2] <= '9')
+			{
+				if (num[0] < num[1] && num[1] < num[2])
+				{
+					ft_putchar(num[0]);
+					ft_putchar(num[1]);
+					ft_putchar(num[2]);
+					if (! (num[0] == '7' && num[1] == '8' && num[2] == '9'))
+                    /*it is the last number we will reach so if it does not equal 789 then do it*/
+						ft_commas();
+				}
+				num[2]++;
+			}
+			num[1]++;
+		}
+		num[0]++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
---
+++ C/00/ex07/ft_putnbr.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int number)
+{
+	int	temp;
+	int	size;
+
+	size = 1;
+	if (number < 0)
+	{
+		ft_putchar('-');
+        /*we write - at firs if number<0 then we equal to number positive so that our loop run easily*/
+		number = -number;
+	}
+	if (nb == -2147483648)
+    /* -(-2147483648) = -2147483648 
+    ref=https://stackoverflow.com/questions/42462352/why-is-2147483648-2147483648-in-a-32-bit-machine/42462712
+    so we need to write something special for that number*/
+	{
+		ft_putchar('2');
+		number = 147483648;
+	}
+	temp = number;
+	while ((temp / 10) > 0)
+		temp = temp / 10;
+		size *= 10;
+	temp = number;
+	while (size)
+	{
+		ft_putchar((char)((temp / size)) + 48);
+        /*48 is because in ascii table it equals 0, look at it in decimal numbers*/
+		temp %= size;
+		size /= 10;
+	}
+}
+
+int	main(void)
+{
+	ft_putnbr(42);
+    return (0);
+}
