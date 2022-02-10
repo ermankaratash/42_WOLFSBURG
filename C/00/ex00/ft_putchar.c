2bd541cb0490b3798696a0e63b434ca09f140501
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2022-02-10 20:57:19 +0100

    remove a.exe

--- C/00/ex00/ft_putchar.c
+++ C/00/ex00/ft_putchar.c
@@ -4,9 +4,9 @@
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
-	/*1 for standart output to be printed, can be also a file where printed*/
-	/*& for addressing to variable and c is variable*/
-	/*1 for how many bytes*/
+/*1 for standart output to be printed, can be also a file where printed
+*& for addressing to variable and c is variable
+1 for how many bytes*/
 }
 
 int	main(void)
@@ -14,3 +14,5 @@ int	main(void)
 	ft_putchar('a');
 	return (0);
 }
+/* write(descriptor, buffer pointer, number of bytes to write)
+		descriptor: 0=standart input(keyboard), 1=standart output(screen), 2= standart error message output(also screen)*/
