185a57ac8bcbd39a88c1de74dec7a6589f4ec4e6
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2022-02-17 23:14:17 +0100

    correction

--- C/02/ex09/ft_strcapitalize.c
+++ C/02/ex09/ft_strcapitalize.c
@@ -5,39 +5,82 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+int	ft_char_is_alpha(char c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
 
+int	ft_char_is_alpha_lower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
 
-char *ft_strcapitalize(char *str)
+int	ft_char_is_alpha_upper(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_char_is_numeric(char c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+char	*ft_strcapitalize(char *str)
 {
 	int	i;
-    int j;
+	int	j;
 
 	i = 0;
-    j = 1;
-
-    while (*(str + i))
-    {
-        if ((*(str + i) >= 'a' && *(str + i) <= 'z') && j == 1)
-        {
-            *(str + i) = *(str + i) - 32;
-            j = 0;
-        }
-        i++;
-        if (!(*(str + i) >= 'a' && *(str + i) <= 'z'))
-        {
-            j = 1;
-        }
-        if (*(str + i) >= '0' && *(str + i) <= '9')
-        {
-            j = 0;
-        }
-    }
-    return (str);
+	j = 1;
+	while (str[i])
+	{
+		if (ft_char_is_alpha_lower(str[i]) && j == 1)
+		{
+			str[i] = str[i] - 32;
+			j = 0;
+			i++;
+		}
+		while (str[i] && ft_char_is_alpha(str[i]))
+		{
+			if (ft_char_is_alpha_upper(str[i]) && j == 0)
+				{
+					str[i] = str[i] + 32;			
+				}
+			i++;
+		}
+		j = 1;
+		if (ft_char_is_numeric(str[i]))
+		{
+			j = 0;
+		}
+		i++;
+	}
+	return (str);
 }
 
 int main(void)
 {
-    char    str[] = {"salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un"};
+    char    str[] = {"-[I=A@)T@:E)9CFSqx;$F0';Q#R2YL^T&K$saLuT, Comment tu vas ? 42mots quarante-deux; cinquante+et+un\n"};
     int i;
     char *p;
 
