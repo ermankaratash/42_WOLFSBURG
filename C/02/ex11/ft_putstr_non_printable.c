b1e12d706e377f05d8557dbe72b5608643297938
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2022-02-20 00:07:48 +0100

    first commit

--- C/02/ex11/ft_putstr_non_printable.c
+++ C/02/ex11/ft_putstr_non_printable.c
@@ -5,29 +5,34 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void ft_putstr_non_printable(char *str)
+int	ft_char_is_printable(char c)
 {
-	int	    i;
-    char    *hexadecimals;
+	if (c >= 32 && c <= 126)
+		return (1);
+	return (0);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int		i;
+	char	*hexadecimals;
 
 	i = 0;
-    hexadecimals = "0123456789abcdef";
-
-    while (*(str + i))
-    {
-        if (*(str + i) < 32 && *(str + i) >= 0)
-        /*nonprintable characters are between 0 and 31 in ascii table*/
-        {
-            ft_putchar('\\');
-            ft_putchar(hexadecimals[*(str + i) / 16]);
-            ft_putchar(hexadecimals[*(str + i) % 16]);
-        }
-        else
-        {
-            ft_putchar(*(str + i));
-        }
-        i++;
-    }
+	hexadecimals = "0123456789abcdef";
+	while (str[i])
+	{
+		if (!(ft_char_is_printable(str[i])))
+		{
+			ft_putchar('\\');
+			ft_putchar(hexadecimals[str[i] / 16]);
+			ft_putchar(hexadecimals[str[i] % 16]);
+		}
+		else
+		{
+			ft_putchar(str[i]);
+		}
+		i++;
+	}
 }
 
 int main(void)
