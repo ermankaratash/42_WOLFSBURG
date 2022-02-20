e59fc5b17a91d4bf6b621549fdc0f72881a2e84d
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2022-02-20 23:26:51 +0100

    update on ex00-ex01, or s2[i] added

--- C/03/ex00/ft_strcmp.c
+++ C/03/ex00/ft_strcmp.c
@@ -7,7 +7,7 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i])
+	while (s1[i] || s2[i])
 	{
 		if (s1[i] == s2[i])
 			i++;
@@ -37,7 +37,7 @@ int	main(void)
 	s4 = "ab0defghi";
 	printf("s1: %s, s2: %s, s3: %s, s4: %s, s5: %s\n", s1, s2, s3, s4, s5);
 	printf("cmp_12 = %d, og_cmp_12 = %d\n", ft_strcmp(s1, s2), strcmp(s1, s2));
-	printf("cmp_13 = %d, og_cmp_13 = %d\n", ft_strcmp(s1, s3), strcmp(s1, s3));
+	printf("cmp_13 = %d, og_cmp_13 = %d\n", ft_strcmp(s2, s3), strcmp(s2, s3));
 	printf("cmp_14 = %d, og_cmp_14 = %d\n", ft_strcmp(s1, s4), strcmp(s1, s4));
 	printf("cmp_15 = %d, og_cmp_15 = %d\n", ft_strcmp(s1, s5), strcmp(s1, s5));
 	return(0);
